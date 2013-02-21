83001eb1bd812ef39be239403a3e39b7626dfea3
Hendrik Wallbaum <gemret@gmail.com>
2013-02-21 16:17:12 +0100

    Initiales Setup

---
+++ src/de/lias/jumpnrun/Main.java
@@ -0,0 +1,27 @@
+package de.lias.jumpnrun;
+
+/**
+ * Dies ist die Main-Klasse, sie wird aufgerufen, sobald das Programm gestartet wird.
+ * @author Hendrik
+ *
+ */
+public class Main
+{
+	/**
+	 * Die Main-Methode, die bei jedem Start aufgerufen wird.
+	 * @param args
+	 */
+	public static void main(String args[]){
+		Main main = new Main();
+		main.Starten();
+	}
+
+	/**
+	 * Die eigentliche Start-Methode.
+	 * Sie wird von "main" aufgerufen und startet die Anwendung.
+	 */
+	private void Starten(){
+		
+	}
+	
+}
